7c137a87d0cb09e8e9290bdf02f2aa7dc812ff28
otondonicolas <n.otondo.pineyro@davinci.edu.ar>
2025-06-07 15:41:36 -0300

    railway config

--- src/main/java/com/tesis/aike/AikeApplication.java
+++ src/main/java/com/tesis/aike/AikeApplication.java
@@ -1,12 +1,23 @@
 package com.tesis.aike;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AikeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AikeApplication.class, args);
+
+
     }
+    @Bean
+    public CommandLineRunner runner() {
+        return args -> {
+            System.out.println(">>> Aike está levantado.");
+        };
+    }
+
 }
