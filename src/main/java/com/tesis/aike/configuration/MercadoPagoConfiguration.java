b4ba0a117c933363108d56e74e350fabdfcbaee2
otondonicolas <n.otondo.pineyro@davinci.edu.ar>
2025-06-07 15:58:20 -0300

    railway config

--- src/main/java/com/tesis/aike/configuration/MercadoPagoConfiguration.java
+++ src/main/java/com/tesis/aike/configuration/MercadoPagoConfiguration.java
@@ -1,14 +1,17 @@
 package com.tesis.aike.configuration;
 
-import com.mercadopago.MercadoPagoConfig;
 import org.springframework.beans.factory.annotation.Value;
+import com.mercadopago.MercadoPagoConfig;
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
-
 @Configuration
 public class MercadoPagoConfiguration {
+
+    public MercadoPagoConfiguration() {
+        System.out.println(">>> Constructor de MercadoPagoConfiguration");
+    }
 
     @Value("${mercadopago.access-token}")
     private String accessToken;
@@ -20,12 +23,14 @@ public class MercadoPagoConfiguration {
     public void init() {
         try {
             System.out.println(">>> MercadoPago init...");
+            System.out.println(">>> accessToken: " + accessToken);
             MercadoPagoConfig.setAccessToken(accessToken);
             System.out.println(">>> MercadoPago OK");
         } catch (Exception e) {
             System.err.println(">>> Error en MercadoPago init: " + e.getMessage());
         }
     }
+
     @Bean(name = "webHookSecret")
     public String getWebHookSecret() {
         return webHookSecret;
