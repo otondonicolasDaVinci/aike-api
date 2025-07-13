4ba343784d2b01434989065a43bfd581585a62bb
otondonicolasDaVinci <109868679+otondonicolasDaVinci@users.noreply.github.com>
2025-07-13 15:51:09 -0300

    Add image URL support for cabins

--- src/main/java/com/tesis/aike/model/entity/CabinEntity.java
+++ src/main/java/com/tesis/aike/model/entity/CabinEntity.java
@@ -23,6 +23,9 @@ public class CabinEntity {
     @Basic
     @Column(name = "available", nullable = true)
     private Boolean available;
+    @Basic
+    @Column(name = "image_url", nullable = true, length = -1)
+    private String imageUrl;
 
     public Long getId() {
         return id;
@@ -64,17 +67,25 @@ public class CabinEntity {
         this.available = available;
     }
 
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         CabinEntity that = (CabinEntity) o;
-        return id == that.id && capacity == that.capacity && Objects.equals(name, that.name) && Objects.equals(description, that.description) && Objects.equals(available, that.available);
+        return id == that.id && capacity == that.capacity && Objects.equals(name, that.name) && Objects.equals(description, that.description) && Objects.equals(available, that.available) && Objects.equals(imageUrl, that.imageUrl);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, capacity, description, available);
+        return Objects.hash(id, name, capacity, description, available, imageUrl);
     }
 
     public boolean isPresent() {
